54de0564535c2f3360c7b0be0c4197cf5fbdb072
squidKnight <michellelieberson@yahoo.com>
2020-03-12 19:35:26 -0400

    consts for port numbers on the ardunio

---
+++ portDefs.h
@@ -0,0 +1,16 @@
+/*
+Written by squidKnight
+Last modified: 03/13/20
+Purpose: holds all of the needed port numbers on the Ardunio and gives them aliases
+*/
+
+//sensors
+#define IR_LEFT 0
+#define IR_RIGHT 0
+#define IR_FRONT 0
+#define ENCODER_LEFT 0
+#define ENCODER_RIGHT 0
+
+//motors
+#define MOTOR_LEFT 0
+#define MOTOR_RIGHT 0
