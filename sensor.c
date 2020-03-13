c57829e95f34696d307073e23122daa4fc60908c
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-03-13 15:37:52 -0400

    Create sensor.c
    Script for sensors.

---
+++ sensor.c
@@ -0,0 +1,18 @@
+*
+Written by Mathazzar
+Last modified: 03/13/20
+Purpose: allow for easy control of sensors
+Status: UNFINISHED, NOT TESTED
+*/
+
+#include "portDefs.h"
+
+int getIR(int sensor) //ask which infra-red sensor to read
+{
+  //constant is using best-fit line, needs to be adjusted based on testing or apply actuall graph from datasheet of GP2Y0A21YK0F
+  return (27/analogRead(sensor));\\27V*cm constant/output
+}
+
+int getUS(int sensor) //ask which ultra-sonic sensor to read
+{
+  
