51b2ef5a5adf54c4867c869c606312eec5d8e09f
squidKnight <michellelieberson@yahoo.com>
2020-03-13 00:21:59 -0400

    added encoder CLK and DT ports to portDefs.h, also created forward() in motor.c

--- portDefs.h
+++ portDefs.h
@@ -2,14 +2,19 @@
 Written by squidKnight
 Last modified: 03/13/20
 Purpose: holds all of the needed port numbers on the Ardunio and gives them aliases
+Status: UNFINISHED, NOT TESTED
 */
 
-//sensors
+//IR sensors
 #define IR_LEFT 0
 #define IR_RIGHT 0
 #define IR_FRONT 0
-#define ENCODER_LEFT 0
-#define ENCODER_RIGHT 0
+
+//quadrature encoders
+#define QUAD_LEFT_CLK 0
+#define QUAD_LEFT_DT 0
+#define QUAD_RIGHT_CLK 0
+#define QUAD_RIGHT_DT 0
 
 //motors
 #define MOTOR_LEFT 0
