2aebbeb68fe176d4bae10f78524eb85725b1fe2a
squidKnight <michellelieberson@yahoo.com>
2020-03-15 17:46:04 -0400

    main exclusively for simulator

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+Written by squidKnight
+Last modified: 03/15/20
+Purpose: run the program on the simulator, which requires a main.c
+
+NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
+*/
+
+void scan();
+
+int main()
+{
+	scan();
+	return 0;
+}
