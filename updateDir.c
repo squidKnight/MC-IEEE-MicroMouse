a5c8f2b1492104b201954a948eef8d7788036936
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-05-13 12:41:01 -0400

    Comment out debug code in updateDir()
    Comment out unnecessary debug code to streamline error-checking for current errors.

--- updateDir.c
+++ updateDir.c
@@ -1,10 +1,11 @@
 /*
 Written by Mathazzar
-Last modified: 04/24/20
+Last modified: 05/13/20
 Purpose: update the direction the micromouse is facing
+Status: FINISHED, TESTED
 */
 
-#include <stdbool.h>	
+#include <stdbool.h>
 #include <stdio.h>
 
 void simLog(char* text); //modified from main.c in mms example (https://github.com/mackorone/mms-c)
@@ -24,36 +25,36 @@ NOTES:
 short int updateDir(short int direction, short int relativeChange)
 {
 	short int dire = direction;
-	fprintf(stderr, "DIRECTION: %d\n", dire);
-	fflush(stderr);
+	//fprintf(stderr, "DIRECTION: %d\n", dire);
+	//fflush(stderr);
 	switch (relativeChange)
 	{
 	case 0: //straight
-		simLog("Turning Straight?");
+		//simLog("Turning Straight?");
 		break;
 	case 1: //turn right
-		simLog("Turning Right");
+		//simLog("Turning Right");
 		dire++;
 		if (dire > 3)
 			dire -= 4;
-		fprintf(stderr, "DIRECTION: %d\n", dire);
-		fflush(stderr);
+		//fprintf(stderr, "DIRECTION: %d\n", dire);
+		//fflush(stderr);
 		break;
 	case 2: //about face
-		simLog("About Face");
+		//simLog("About Face");
 		dire += 2;
 		if (dire > 3)
 			dire -= 4;
-		fprintf(stderr, "DIRECTION: %d\n", dire);
-		fflush(stderr);
+		//fprintf(stderr, "DIRECTION: %d\n", dire);
+		//fflush(stderr);
 		break;
 	case 3: //turn left
-		simLog("Turning Left");
+		//simLog("Turning Left");
 		dire--;
 		if (dire < 0)
 			dire += 4;
-		fprintf(stderr, "DIRECTION: %d\n", dire);
-		fflush(stderr);
+		//fprintf(stderr, "DIRECTION: %d\n", dire);
+		//fflush(stderr);
 		break;
 	default:
 		simLog("DIRECTION UPDATE ERROR");
