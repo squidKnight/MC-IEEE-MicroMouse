7a79d7151aba8b74fa439cc295a332f7c368769c
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-05-25 23:17:07 -0400

    Discarded unecessary variables in pathChoose()
    Discarded explorability variables since pathChooseAlt() renders then moot.

--- pathChoose.c
+++ pathChoose.c
@@ -1,6 +1,6 @@
 /*
 Written by Mathazzar
-Last modified: 05/24/20
+Last modified: 05/25/20
 Purpose: choose next direction to take at a revisited node.
 Status: FINISHED, TESTED
 */
@@ -33,7 +33,7 @@ CAUTION:
 short int pathChoose(int nodeList[NODES][DATA], int nodeCurrent, short int direction)
 {
 	short int dire = direction;
-	short int front, right, back, left, expF, expR, expB, expL;
+	short int front, right, back, left;
 	switch (dire) //set directions for current orientation
 	{
 	case 0:
@@ -41,57 +41,41 @@ short int pathChoose(int nodeList[NODES][DATA], int nodeCurrent, short int direc
 		right = NODEID_R;
 		back = NODEID_B;
 		left = NODEID_L;
-		expF = EXP_T;
-		expR = EXP_R;
-		expB = EXP_B;
-		expL = EXP_L;
 		break;
 	case 1:
 		front = NODEID_R;
 		right = NODEID_B;
 		back = NODEID_L;
 		left = NODEID_T;
-		expF = EXP_R;
-		expR = EXP_B;
-		expB = EXP_L;
-		expL = EXP_T;
 		break;
 	case 2:
 		front = NODEID_B;
 		right = NODEID_L;
 		back = NODEID_T;
 		left = NODEID_R;
-		expF = EXP_B;
-		expR = EXP_L;
-		expB = EXP_T;
-		expL = EXP_R;
 		break;
 	case 3:
 		front = NODEID_L;
 		right = NODEID_T;
 		back = NODEID_R;
 		left = NODEID_B;
-		expF = EXP_L;
-		expR = EXP_T;
-		expB = EXP_R;
-		expL = EXP_B;
 		break;
 	}
-	fprintf(stderr, "nodeCurrent: %d, nodeID: %d,\t\tfront: %d, right: %d, back: %d, left: %d, expF: %d, expR: %d, expB: %d, expL: %d\n", nodeCurrent, nodeList[nodeCurrent][NODEID], nodeList[nodeCurrent][front], nodeList[nodeCurrent][right], nodeList[nodeCurrent][back], nodeList[nodeCurrent][left], nodeList[nodeCurrent][expF], nodeList[nodeCurrent][expR], nodeList[nodeCurrent][expB], nodeList[nodeCurrent][expL]);
+	fprintf(stderr, "nodeCurrent: %d, nodeID: %d,\t\tfront: %d, right: %d, back: %d, left: %d\n", nodeCurrent, nodeList[nodeCurrent][NODEID], nodeList[nodeCurrent][front], nodeList[nodeCurrent][right], nodeList[nodeCurrent][back], nodeList[nodeCurrent][left]);
 	fflush(stderr);
 
 	//Choose next available route not previously traveled
-	if ((nodeList[nodeCurrent][front] == 0) && (nodeList[nodeCurrent][expF] == 0)) //if front unexplored
+	if (nodeList[nodeCurrent][front] == 0) //if front unexplored
 	{
 		simLog("Picking unexplored straight.");
 	}
-	else if ((nodeList[nodeCurrent][left] == 0) && (nodeList[nodeCurrent][expL] == 0)) //if left unexplored
+	else if (nodeList[nodeCurrent][left] == 0) //if left unexplored
 	{
 		simLog("Picking unexplored left.");
 		API_turnLeft();
 		dire = updateDir(dire, 3);
 	}
-	else if ((nodeList[nodeCurrent][right] == 0) && (nodeList[nodeCurrent][expR] == 0)) //if right unexplored
+	else if (nodeList[nodeCurrent][right] == 0) //if right unexplored
 	{
 		simLog("Picking unexplored right.");
 		API_turnRight();
