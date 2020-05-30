2a7691889d7effacd1250a53a8e62e9c2200219a
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-05-29 21:10:41 -0400

    Add debug code to main()

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 /*
 Written by squidKnight, Mathazzar
-Last modified: 04/24/20
+Last modified: 05/29/20
 Purpose: run the program on the simulator, which requires a main.c
 
 NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
 */
 
 #include "mouseDefs.h"
+#include <stdio.h>
 
 int nodeList[NODES][DATA];
 
@@ -17,5 +18,10 @@ int main()
 {
 	nodeInit(nodeList);
 	scan(nodeList);
+	for (int i = 0; i < NODES; i++)
+	{
+		fprintf(stderr, "%d;\t%d, %d, %d;\t%d, %d, %d, %d;\t%d, %d, %d, %d\n", i, nodeList[i][NODEID], nodeList[i][DIST], nodeList[i][NODEID_P], nodeList[i][NODEID_T], nodeList[i][NODEID_R], nodeList[i][NODEID_B], nodeList[i][NODEID_L], nodeList[i][DIST_T], nodeList[i][DIST_R], nodeList[i][DIST_B], nodeList[i][DIST_L]);
+		fflush(stderr);
+	}
 	return 0;
 }
