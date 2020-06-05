2a54fb99a699b80a44ceb3d49edbb0d9fd9920f2
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-06-05 18:51:42 -0400

    Shifted data upstream from scan.c
    Change data structure to use short int instead of int, shifted position array and direction variables up so they can be passed to later functions.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 Written by squidKnight, Mathazzar
-Last modified: 06/04/20
+Last modified: 06/05/20
 Purpose: run the program on the simulator, which requires a main.c
 
 NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
@@ -9,15 +9,18 @@ NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
 #include "mouseDefs.h"
 #include <stdio.h>
 
-int nodeList[NODES][DATA];
+short int nodeList[NODES][DATA];
 
-void nodeInit(int nodeList[NODES][DATA]); //initialize nodeList
-void scan(int nodeList[NODES][DATA]); //scans the maze
+void nodeInit(short int nodeList[NODES][DATA]); //initialize nodeList
+void scan(short int nodeList[NODES][DATA], short int position[2], short int direction); //scans the maze
 
 int main()
 {
+	short int position[2] = { 0, 0 };
+	short int direction = 0;
+
 	nodeInit(nodeList);
-	scan(nodeList);
+	scan(nodeList, position, direction);
 	for (int i = 0; i < NODES; i++)
 	{
 		fprintf(stderr, "%d;\t%d, %d, %d, %d;\t%d, %d, %d, %d\n", i, nodeList[i][NODEID], nodeList[i][NODEID_T], nodeList[i][NODEID_R], nodeList[i][NODEID_B], nodeList[i][NODEID_L], nodeList[i][DIST_T], nodeList[i][DIST_R], nodeList[i][DIST_B], nodeList[i][DIST_L]);
