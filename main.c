c19181d44dc2d2495f128cc593bb706150ab45a3
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-06-04 22:54:19 -0400

    Discard unnecessary variables in main.c
    Discard unnecessary backpath and distance from start variables in main.c as stackPath() renders them moot.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 Written by squidKnight, Mathazzar
-Last modified: 05/29/20
+Last modified: 06/04/20
 Purpose: run the program on the simulator, which requires a main.c
 
 NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
@@ -20,7 +20,7 @@ int main()
 	scan(nodeList);
 	for (int i = 0; i < NODES; i++)
 	{
-		fprintf(stderr, "%d;\t%d, %d, %d;\t%d, %d, %d, %d;\t%d, %d, %d, %d\n", i, nodeList[i][NODEID], nodeList[i][DIST], nodeList[i][NODEID_P], nodeList[i][NODEID_T], nodeList[i][NODEID_R], nodeList[i][NODEID_B], nodeList[i][NODEID_L], nodeList[i][DIST_T], nodeList[i][DIST_R], nodeList[i][DIST_B], nodeList[i][DIST_L]);
+		fprintf(stderr, "%d;\t%d, %d, %d, %d;\t%d, %d, %d, %d\n", i, nodeList[i][NODEID], nodeList[i][NODEID_T], nodeList[i][NODEID_R], nodeList[i][NODEID_B], nodeList[i][NODEID_L], nodeList[i][DIST_T], nodeList[i][DIST_R], nodeList[i][DIST_B], nodeList[i][DIST_L]);
 		fflush(stderr);
 	}
 	return 0;
