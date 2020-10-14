10555d14af4d72d53e30f617b2c83432d684adcf
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-10-14 17:15:25 -0400

    Reimplement main()
    Minor edits to ensure adherence to new data structure. Reduced a for-loop to 4 independent checks that are now absolute, and don't require checking the whole nodeList array for the maze's center nodeIDs.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 Written by squidKnight, Mathazzar
-Last modified: 10/09/20
+Last modified: 10/14/20
 Purpose: run the program on the simulator, which requires a main.c
 
 NOTE: ONLY USED FOR SIMULATOR, NOT FOR USE ON ARDUINO!!
@@ -17,9 +17,9 @@ void nodeInit(bool nodeList[NODES][DATA]); //initialize nodeList
 short int scan(bool nodeList[NODES][DATA], short int position[2], short int direction); //scans the maze
 void pathTree(bool nodeList[NODES][DATA], short int holdList[NODES], short int nodeCurrent); //generates minimum spanning tree from nodeCurrent
 void stackPath(bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int holdList[NODES], short int nodeCurrent, short int nodeNext); //finds shortest path to a given node from a given node using a minimum spanning tree
-short int getID(bool position[2]); //generates unique ID for a node based on it's x-y coords
+short int getID(short int position[2]); //generates unique ID for a node based on it's x-y coords
 short int rtb(bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext); //moves the micromouse to nodeNext from anywhere in the maze
-short int updateDir(bool direction, short int relativeChange); //updates the direction the micromouse is facing
+short int updateDir(short int direction, short int relativeChange); //updates the direction the micromouse is facing
 
 int main()
 {
@@ -52,20 +52,33 @@ int main()
 	nodeCurrent = getID(position);
 	pathTree(nodeList, holdList, nodeCurrent);
 	short int minDist = INFINITY;
-	for (int i = 0; i < NODES; i++)
+	if (holdList[119] < minDist) //check distance to nodeID 120 (bottom left center)
 	{
-		if ((i + 1 == 120) || (i + 1 == 121) || (i + 1 == 136) || (i + 1 == 137))
-			if (holdList[i] < minDist)
-			{
-				minDist = holdList[i];
-				nodeCurrent = i + 1; //reuse nodeCurrent as nodeNext
-			}
+		minDist = holdList[119];
+		nodeCurrent = 119; //reuse nodeCurrent as nodeNext
+	}
+	if (holdList[120] < minDist) //check distance to nodeID 121 (bottom right center)
+	{
+		minDist = holdList[120];
+		nodeCurrent = 120; //reuse nodeCurrent as nodeNext
+	}
+	if (holdList[135] < minDist) //check distance to nodeID 136 (top left center)
+	{
+		minDist = holdList[135];
+		nodeCurrent = 135; //reuse nodeCurrent as nodeNext
+	}
+	if (holdList[136] < minDist) //check distance to nodeID 137 (top right center)
+	{
+		minDist = holdList[136];
+		nodeCurrent = 136; //reuse nodeCurrent as nodeNext
 	}
 	if (minDist == INFINITY)
 		simLog("FATAL ERROR: center was not found.");
 	else
 	{
 		stackPath(nodeList, pathList, holdList, getID(position), nodeCurrent);
+		//^^^^final step to define points on 16x16 grid for smoothing, further code here can only be done for the simulator's sake.
+		//Final product code can't be tested in the simulator beyond this point, and requires either a physical micromouse and maze, or other simulation software to test with.
 		direction = rtb(nodeList, pathList, position, direction, nodeCurrent);
 	}
 
