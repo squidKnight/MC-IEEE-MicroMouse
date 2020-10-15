fccbd111969fee4ee09641d40736460974a34980
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-10-15 12:02:26 -0400

    Add files via upload
    Split duplicate private functions into their own file as a set.

---
+++ nodeDefs.h
@@ -0,0 +1,13 @@
+/*
+Written by Mathazzar
+Last modified: 10/15/20
+Purpose: list functions for finding adjacent nodes.
+Status: FINISHED, NOT TESTED
+Notes: functions currently declared in nodeCheck.c
+*/
+
+short int node(short int nodeID);
+short int node_T(short int nodeID);
+short int node_R(short int nodeID);
+short int node_B(short int nodeID);
+short int node_L(short int nodeID);
