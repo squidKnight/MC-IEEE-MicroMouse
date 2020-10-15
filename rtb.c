e016e8acf7420664654791cb466f91ba2ac8c1af
Mathazzar <62147177+Mathazzar@users.noreply.github.com>
2020-10-15 13:51:09 -0400

    Reimplement rtb.c
    Switched to nodeList array's new data structure, removed unused code, added private functions to reimplement previous patch's functions not used elsewhere.

--- rtb.c
+++ rtb.c
@@ -1,31 +1,29 @@
 /*
 Written by Mathazzar
-Last modified: 10/09/20
+Last modified: 10/15/20
 Purpose: return the micromouse from an arbitrary point in the maze to nodeNext.
 */
 
-#include <stdbool.h>
 #include <stdio.h>
 #include "API.h" //only needed for simulator use
 #include "mouseDefs.h"
+#include "nodeDefs.h"
 
 void simLog(char* text); //modified from main.c in mms example (https://github.com/mackorone/mms-c)
-short int nodeCheck(); //checks to see if the current location is a node
 short int changeDir(short int direction, short int newDirection); //updates the direction the micromouse is facing
 short int getID(short int position[2]); //generates unique ID for a node based on it's x-y coords
-short int stackCheck(short int nodeList[NODES][DATA], short int nodeCurrent); //find rank of nodeCurrent if it exists in nodeList
-short int directionNext(short int nodeCurrent[DATA], short int nodeNext); //identifies the direction of the current node's backpath to prevent longer path to the next node being taken
-short int pathCheck(short int position[2], short int *dire); //Move's micromouse to the next node
 void updatePos(short int position[2], short int direction, short int dist); //updates the position of the micromouse
 
-/*short int rtb(short int nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext)
-INPUTS: short int nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext
+static short int directionNext(bool nodeCurrent[DATA], short int rank, short int pathNext);
+
+/*short int rtb(bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext)
+INPUTS: bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext
 	nodeList: the nodeList array.
 	pathList: the pathList array.
 	position: the position array.
 	direction: the current orientation of the micromouse.
-	nodeNext: the desired endpoint.
-RETURNS: short int nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int dire
+	nodeNext: nodeID of the desired endpoint.
+RETURNS: bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int dire
 	nodeList: should return unmodified.
 	pathList: should return unmodified.
 	position: modifies the position array directly.
@@ -38,17 +36,17 @@ NOTES:
 CAUTION:
 	Interacts with the nodeList array directly.
 	Interacts with the pathList array directly.
-	Manipulates the position array directly.
+	Manipulates the position array directly via updatePos().
 */
-short int rtb(short int nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext)
+short int rtb(bool nodeList[NODES][DATA], short int pathList[NODES / 8], short int position[2], short int direction, short int nodeNext)
 {
 
 	simLog("Returning to base...");
 	short int dire = direction;
-	short int nodeCurrent = stackCheck(nodeList, pathList[0]);
+	short int nodeCurrent = node(pathList[0] - 1);  //find position in nodeList array of the start of the pathList
 	short int nodeID = getID(position);
 
-	if (nodeID != nodeList[nodeCurrent][NODEID])
+	if (nodeID != (node(nodeCurrent) + 1))
 	{
 		simLog("FATAL ERROR: NodeID missmatch.");
 		return dire;
@@ -70,7 +68,7 @@ short int rtb(short int nodeList[NODES][DATA], short int pathList[NODES / 8], sh
 			break;
 		}
 
-		dire = changeDir(dire, directionNext(nodeList[nodeCurrent], pathList[++x]));
+		dire = changeDir(dire, directionNext(nodeList[nodeCurrent], nodeCurrent, pathList[++x]));
 		if (dire == -1)
 		{
 			simLog("FATAL ERROR: backpath not among available routes.");
@@ -81,7 +79,6 @@ short int rtb(short int nodeList[NODES][DATA], short int pathList[NODES / 8], sh
 		{
 			API_moveForward();
 			updatePos(position, dire, 1);
-			pathCheck(position, &dire);
 
 			nodeID = getID(position);
 			nodeCurrent = stackCheck(nodeList, nodeID);
@@ -93,5 +90,22 @@ short int rtb(short int nodeList[NODES][DATA], short int pathList[NODES / 8], sh
 		}
 	}
 
+	return dire;
+}
+
+static short int directionNext(bool nodeCurrent[DATA], short int rank, short int pathNext)
+{
+	short int dire = -1;
+	short int next = node(pathNext - 1);
+	if ((node_T(rank) == next) && nodeCurrent[WAL_T])
+		dire = 0;
+	else if ((node_R(rank) == next) && nodeCurrent[WAL_R])
+		dire = 1;
+	else if ((node_B(rank) == next) && nodeCurrent[WAL_B])
+		dire = 2;
+	else if ((node_L(rank) == next) && nodeCurrent[WAL_L])
+		dire = 3;
+	else
+		simLog("CRITICAL ERROR: nodeNext is not adjacent to nodeCurrent");
 	return dire;
 }
